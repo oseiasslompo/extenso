1a010831c7e6a6fbac4564c28fa7c5c703f04ade
Oseias Slompo <oseiasslompo@gmail.com>
2021-08-08 12:48:37 -0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dd046daf9059dc6dd5d74dad9ee52b1e7b4d619d',
+        'name' => 'oseias/extenso',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'oseias/extenso' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dd046daf9059dc6dd5d74dad9ee52b1e7b4d619d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
